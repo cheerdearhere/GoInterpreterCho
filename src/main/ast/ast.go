bbd26ecf5136be17aa6121ad38aa53146af830a1
dreamik <dream-ik89@naver.com>
2025-01-18 09:29:03 +0900

    feat: make ast for Pratt parser(ready for pratt parser)

--- src/main/ast/ast.go
+++ src/main/ast/ast.go
@@ -1,9 +1,14 @@
 package ast
 
-import "GoInterpreter/src/main/token"
+import (
+	"GoInterpreter/src/main/token"
+	"bytes"
+)
 
+/*interface*/
 type Node interface {
 	TokenLiteral() string
+	String() string
 }
 
 type Statement interface {
@@ -16,6 +21,7 @@ type Expression interface {
 	expressionNode()
 }
 
+/*구문 정의*/
 type Program struct {
 	Statements []Statement
 }
@@ -28,7 +34,6 @@ func (p *Program) TokenLiteral() string {
 	}
 }
 
-/*구문 정의*/
 type LetStatement struct {
 	Token token.Token //token.LET
 	Name  *Identifier
@@ -53,3 +58,56 @@ type ReturnStatement struct {
 
 func (rs *ReturnStatement) statementNode()       {}
 func (rs *ReturnStatement) TokenLiteral() string { return rs.Token.Literal }
+
+type ExpressionStatement struct {
+	Token      token.Token //표현식의 첫번째 토큰
+	Expression Expression
+}
+
+func (es *ExpressionStatement) statementNode()       {}
+func (es *ExpressionStatement) TokenLiteral() string { return es.Token.Literal }
+
+/*for String method overloading start*/
+func (p *Program) String() string {
+	var out bytes.Buffer
+
+	for _, s := range p.Statements {
+		out.WriteString(s.String())
+	}
+
+	return out.String()
+}
+func (ls *LetStatement) String() string {
+	var out bytes.Buffer
+
+	out.WriteString(ls.TokenLiteral() + " ")
+	out.WriteString(ls.Name.String())
+	out.WriteString(" = ")
+
+	if ls.Value != nil {
+		out.WriteString(ls.Value.String())
+	}
+
+	out.WriteString(";")
+
+	return out.String()
+}
+func (rs *ReturnStatement) String() string {
+	var out bytes.Buffer
+
+	out.WriteString(rs.TokenLiteral() + " ")
+	if rs.ReturnValue != nil {
+		out.WriteString(rs.ReturnValue.String())
+	}
+	out.WriteString(";")
+	return out.String()
+}
+func (es *ExpressionStatement) String() string {
+	if es.Expression != nil {
+		return es.Expression.String()
+	}
+	return ""
+}
+func (i *Identifier) String() string { return i.Value }
+
+/*for String method overloading end*/
